ee9a7481017d615b232203d38c3d3fd0d942c509
CSwigg <swiggum2@wisc.edu>
2020-08-25 17:19:00 -0500

    update

--- analysis2.py
+++ analysis2.py
@@ -24,24 +24,27 @@ def cluster_age_analysis(df, cluster_label : int, cluster_name : str, cost_funct
 
 
 df = pd.read_csv('/Users/cam/Desktop/astro_research/orion/apogee_gaia_sample4.csv')
-df = df.loc[df['probability'] > .2]
+df = df.loc[df['probability'] > .1]
 #df = df.loc[df['phot_bp_mean_mag'] - df['phot_rp_mean_mag'] < 2.2]
 
 #age_obpNear = cluster_age_analysis(df, 3, 'OBP-near', 'astrometric_photometric')
+#age_briceno = cluster_age_analysis(df, 1, 'Briceño-1', 'astrometric_photometric')
+#age_far = cluster_age_analysis(df, 19, 'OBP-far', 'astrometric_photometric')
 #age_ngc1980 = cluster_age_analysis(df, 5, 'NGC 1980', 'astrometric_photometric')
 #age_lOri = cluster_age_analysis(df, 4, 'Lambda Ori', 'astrometric_photometric')
-age_oriY = cluster_age_analysis(df, 2, 'Orion Y', 'astrometric_photometric')
+#age_oriY = cluster_age_analysis(df, 2, 'Orion Y', 'astrometric_photometric')
+#age_7 = cluster_age_analysis(df, 7, 'Orion Y', 'astrometric_photometric')
 
 
-#groups = df.groupby(['label'])
-#df_list = []
-#for label, df_group in groups:
-#    print(label)
-#    age, discard = cluster_age_analysis(df, label, 'cluster', 'astrometric_photometric')
-#    age_array = np.ones(len(df_group))*age
-#    df_group['age'] = age_array
-#    df_list.append(df_group)
-#    
+groups = df.groupby(['label'])
+df_list = []
+for label, df_group in groups:
+    print(label)
+    age, discard = cluster_age_analysis(df, label, 'cluster', 'astrometric_photometric')
+    age_array = np.ones(len(df_group))*age
+    df_group['age'] = age_array
+    df_list.append(df_group)
+    
 #df_new = pd.concat(df_list)
 #df_new.to_csv('../apogee_gaia_sample4_ages.csv')
 
